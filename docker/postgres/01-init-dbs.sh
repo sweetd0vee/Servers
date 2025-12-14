1f208f04ea42179e910d610cdb7e23b548e3b34f
Arina Goloubitskaya <arinaogo@gmail.com>
2025-12-14 13:40:29 +0300

    added docker for postgres

--- docker/postgres/01-init-dbs.sh
+++ docker/postgres/01-init-dbs.sh
@@ -4,7 +4,7 @@ set -e
 
 # This script creates additional databases. The main database and user
 # are created by the entrypoint script from the environment variables.
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$DB_USER" <<-EOSQL
     CREATE DATABASE server_metrics;
     CREATE DATABASE keycloak;
 EOSQL
